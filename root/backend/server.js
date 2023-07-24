e9bcb65e0f739b6788e6e4f53cffbe6c412fc8a4
Luigi Radano <igiuldj@gmail.com>
2023-07-24 12:15:38 +0200

    Host port can be changed

--- root/backend/server.js
+++ root/backend/server.js
@@ -3,24 +3,30 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 const exp = require("constants");
+const process = require('process');
 
-const app = express();
+let PORT = 3000;
 
+//Dynamic port with command line first argument
+if(process.argv[2] != undefined){
+    PORT = parseInt(process.argv[2]);
+}
+
+//Setup express
+const app = express(); 
+//Allow cross origin reference (to remove in production version)
 app.use(cors());
-
-app.get(["/"], (req, res) =>{
-
-});
-
 
 //Setup post reception for text
 app.use(express.text());
 
+//Load the juno file
 app.post(["/JunoFile"], (req, res) =>{
     console.log("Recieved file")
     fs.writeFile("./data/juno.csv", req.body, "utf8", ()=>{ res.send("") } //Write the file, then send response
 )});
 
+//Send the juno file
 app.get(["/JunoFile"], (req, res) =>{
     console.log("Recieved request")
     if(fs.existsSync("./data/juno.csv")){
@@ -31,12 +37,12 @@ app.get(["/JunoFile"], (req, res) =>{
 //Send assets
 app.use(express.static('../frontend/dist'))
 
-
 //Send to all get requests the main page
 app.get(['/', '/*'], (req, res) => {
+    
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log("Listen on the port 3000...");
+app.listen(PORT, () => {
+    console.log("Listen on the port " + PORT);
 });
