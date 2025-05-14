aa3f6ea04ceb2d977dded8b994e17c8bdb1e0eb9
Luigi Radano <luigi.radano03@gmail.com>
2025-05-14 18:20:21 +0200

    Updated message ID

--- root/frontend/src/components/messageSender/index.tsx
+++ root/frontend/src/components/messageSender/index.tsx
@@ -16,7 +16,7 @@ export const MessageSender= ({locCar}) => {
     }, [])
 
     const _init = () => {
-        const c = new Paho.Client("broker.mqttdashboard.com", Number(8000), "/mqtt", "myClientId" + new Date().getTime());
+        const c = new Paho.Client("147.185.221.27", Number(14943), "/mqtt", "myClientId" + new Date().getTime());
         c.onConnectionLost = _onConnectionLost;
         c.onMessageArrived = _onMessageArrived;
         c.connect({ onSuccess: onConnect });
