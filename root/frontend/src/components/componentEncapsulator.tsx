ea7b82215106e67f0dc21933d3a830b4151db6d6
Luigi Radano <48596120+luigiradano@users.noreply.github.com>
2025-05-17 00:13:58 +0200

    Update componentEncapsulator.tsx

--- root/frontend/src/components/componentEncapsulator.tsx
+++ root/frontend/src/components/componentEncapsulator.tsx
@@ -72,7 +72,7 @@ export const ComponentEncapsulator: React.FC<Props> = ({
   let arrayMessages: number[] = [];
   let client = new Paho.Client(
     "31.97.32.141",
-    Number(9001),
+    Number(1883),
     "/mqtt",
     topicName
   );
