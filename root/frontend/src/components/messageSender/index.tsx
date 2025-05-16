a02f366a342a86d3a49c3c3c87142b704a477e9f
Luigi Radano <48596120+luigiradano@users.noreply.github.com>
2025-05-16 18:42:48 +0200

    Update index.tsx

--- root/frontend/src/components/messageSender/index.tsx
+++ root/frontend/src/components/messageSender/index.tsx
@@ -16,7 +16,7 @@ export const MessageSender= ({locCar}) => {
     }, [])
 
     const _init = () => {
-        const c = new Paho.Client("147.185.221.27", Number(14943), "/mqtt", "myClientId" + new Date().getTime());
+        const c = new Paho.Client("31.97.32.141", Number(9001), "/mqtt", "myClientId" + new Date().getTime());
         c.onConnectionLost = _onConnectionLost;
         c.onMessageArrived = _onMessageArrived;
         c.connect({ onSuccess: onConnect });
